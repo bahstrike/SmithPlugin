be0c55ef8b812941ca4b653a47e7e8fb1c8f2fb7
bahstrike <strike_1337@hotmail.com>
2018-12-23 14:53:32 -0500

    +initial plugin project

---
+++ SmithPlugin.cpp
@@ -0,0 +1,137 @@
+#include <Windows.h>
+#include "smith.h"
+#include <stdio.h>
+
+/*
+================================== =
+Engine Hooks For AArcade FE Support
+================================== =
+
+CreateProceduralTexture(texture_id, width, height, format, pixelData) :
+	-Creates a texture with the given ID using the given pixel data.
+	- Returns a POINTER to the texture that AArcade will ONLY write to during
+	- Called by AArcade to create a texture that is going to be used on an object later on.
+	- The
+
+	CreateProceduralMaterial(material_id) :
+	-Called by AArcade to setup the engine to give OnMaterialBind callbacks related to the material_id.
+	- This material_id will match a material channel used on the dynamic object models.AArcade dynamic object models are created using material_id's that AArcade is expected to register with the engine.  These include, but are not limited to: DynMarquee, DynMarqueeGlow, DynScreen, DynScreenGlow, DynScreenGlowHologram.
+	- The engine needs to notify AArcade when objects are being rendered that use this material_id.
+
+	OnMaterialBind(material_id, object_id) :
+	-AArcade registers which material_id's it wants to listen for MaterialBind events.
+	- Called by the engine when an object is about to be rendered that uses a registered material_id.
+	- The material_id and the object_id are provided as arguments.
+
+	SwapMaterialTexture(material_id, texture_id) :
+	-Called by AArcade during its response to OnMaterialBind.
+	- The engine swaps in the correct pixel data pointer for the texture being used on the object_id by the provided material_id.
+*/
+
+SMITHCALLS *smith = nullptr;
+
+
+extern "C" {
+
+	void __cdecl OnMainLoop(double dt, double truedt)
+	{
+		if (smith == nullptr)
+			return;
+
+
+	}
+
+	void __cdecl OnLevelShutdownPreObject()
+	{
+		if (smith == nullptr)
+			return;
+
+
+	}
+
+	void __cdecl OnLevelShutdownPostObject()
+	{
+		if (smith == nullptr)
+			return;
+
+
+	}
+
+	void __cdecl OnLevelStartPreObject()
+	{
+		if (smith == nullptr)
+			return;
+
+		//smith->ExecuteCOG("PlaySoundLocal(\"activate02.wav\", 1.0, 0.0, 0x00);", 0, 0, 0, 0, 0, 0, 0, 0);
+	}
+
+	void __cdecl OnLevelStartPostObject()
+	{
+		if (smith == nullptr)
+			return;
+
+		char playerModel[16];
+		//smith->ExecuteCOG("GetThingModel(GetLocalPlayerThing());", 0, 0, 0, 0, 0, 0, 0, playerModel);
+
+	}
+
+	int __cdecl OnExecuteCOGVerb(const char* szName, int nNumParams, const char** pszParams, char* szReturn)
+	{
+		if (stricmp(szName, "StrikeStuff") == 0 && nNumParams == 1)
+		{
+			MessageBox(0, pszParams[0], "SmithPlugin", MB_ICONINFORMATION);
+			return 1;// handled
+		}
+
+		return 0;// not handled;  let smith deal with it
+	}
+
+	// do not distribute this algorithm so long as u want to ensure that your mod identity is protected
+	void _smith_authenticate_smithplugin(const char* szName, int& key)
+	{
+		// if you do not have
+	}
+
+	int _smith_formatversion(int major, int minor)
+	{
+		return major * 100 + minor;
+	}
+
+	int __cdecl SmithQueryPlugin(PLUGININFO& p)
+	{
+		// fill out the details
+		strcpy(p.name, "Sample Plugin");
+		strcpy(p.author, "bahstrike");
+		strcpy(p.desc, "mostly an example plugin project. contains demonstrations of things possible with smith engine mod development");
+		p.ver = _smith_formatversion(1, 0);
+		p.smithRequiredVer = SMITHVERSION;
+		_smith_authenticate_smithplugin(p.name, p.authoritykey);
+
+		return 1337;
+	}
+
+	int __cdecl OnInitializePlugin(SMITHCALLS* _smith)
+	{
+		smith = _smith;
+
+
+
+		int thingID = 10;
+		char szThingID[16];
+		sprintf(szThingID, "%d", thingID);
+
+		char szReturn[16];
+
+		//smith->ExecuteCOG("SetThingMass(GetSourceRef(), 999.0);", szThingID, 0, 0, 0, 0, 0, 0, szReturn);
+
+
+		return 1;
+	}
+
+	void __cdecl OnShutdownPlugin()
+	{
+		// our smith interface is no longer valid
+		smith = nullptr;
+	}
+
+}
---
+++ smith.h
@@ -0,0 +1,154 @@
+/*
+		smith plugin API
+
+		basically just create a win32 C DLL which exports regular ass, non-mangled functions.
+		ensure you export the minimum required symbols for smith to recognize u as a valid plugin.
+
+		follow either the sample project, or this header, to use the API. there is no .lib or additional DLL that
+		you are required to link in; just export the right symbols and call the methods of SMITHCALLS in order
+		to manipulate the engine.
+
+		put your dll in the same directory as smith.exe and thats it.
+
+		NOTE:  smith is essentially a singlethreaded engine. therefore, expect all smith -> plugin calls to be
+		done from the main engine thread (unless otherwise noted).  as well, assume that all plugin -> smith calls
+		are NOT thread safe (unless otherwise noted)
+
+		NOTE:  some smith calls may require, or perform optimally, if smith engine is already in the appropriate state.
+		for example, trying to issue renderer calls while not in a renderer-related OnXXXX event.
+		just try to use best judgement.. if there are calls that LOOK like they would be useful to call at any point,
+		its probably safe to do so.  ill try to notate the ones that are definitely safe / not safe but anyway yeah.
+
+		NOTE:  the intended use of this smith.h header is for you to copy it to your project folder. however i do not
+		suggest modifying it. should you decide to update your plugin in the future, just replace smith.h with the new
+		version and rebuild your dll.
+*/
+
+#pragma once
+
+// please use this to populate PLUGININFO::smithRequiredVer.
+#define SMITHVERSION		109
+
+struct PLUGININFO
+{
+	char name[16];			// required: name of ur plugin, eg. "Strikes Mod"
+	char author[16];		// optional: ur name
+	char desc[1024];		// optional: description of plugin
+	int ver;				// required*: version of your plugin. follow a XYY standard wherein X is major and YY is minor version. eg: 207 -> version 2.7     the default provided is 100  (version 1.0)
+	int smithRequiredVer;	// required*: minimum version of smith that your plugin requires to operate in. same version XYY version standard as described above.  please insert the SMITHVERSION constant
+	int authoritykey;		// optional: key value to prevent spoofing;  prevents other ppl from making plugins that masquerade or try to override an official plugin. talk to strike to be provided an authority key. if you dont have an authority key algorithm, leave this value as the value passed in (do not change it! or your plugin will not load)
+};
+
+enum PIXDATAFORMAT
+{
+	RGB24,		// 24-bit rgb;  dword aligned
+	ARGB32,		// 32-bit argb
+};
+
+struct TEXTURE
+{
+	int width;
+	int height;
+	PIXDATAFORMAT format;
+	void* pixeldata;
+	int stride;
+};
+
+extern "C"
+{
+	// it simply just makes sense to allow plugins to freely execute cog;  this means you can do all sorts of crazy stuff in-game
+	// without having to learn a new API and without me having to write one.
+	//
+	// note that the cog script should NOT include a symbols section, nor  code / end  keywords.
+	//
+	// you are responsible for providing the source/sender and params.  your cog script can use the typical
+	// GetSourceRef(),  GetSenderID(), GetParam(0)  etc  to retrieve these values.
+	//
+	// if your cog calls ReturnEx(), this value will be accessible to you via the returned szReturn  value.
+	//
+	// final note: the smith cog VM operates under a Flex symbol type and generally is most compatible as strings.
+	// so for simplicity of interface, all values are strings. you are responsible for formatting integer values into strings
+	// and parsing return values etc.
+	typedef void (__cdecl *dExecuteCOG)(const char* szCOGScript, const char* szSourceRef, const char* szSenderRef, const char* szSenderID, const char* szParam0, const char* szParam1, const char* szParam2, const char* szParam3, char* szReturn);
+
+	// returns 1 if smith game engine is actually running with a level loaded etc, 0 otherwise.
+	// you should probably check this if you decide to try to issue in-game related functions at a weird moment like
+	// ShutdownPlugin (not recommended).  i cant gaurantee it wont crash if u do somethin like that. so basically call all
+	// game stuff in a callback that you know the game is running, or call IsInGame  to check first.
+	typedef int(__cdecl *dIsInGame)();
+
+	// generates a procedural texture object with a unique ID as a name. 
+	//typedef TEXTURE* (__cdecl *dCreateProceduralTexture)(int uniqueID, int width, int height, PIXDATAFORMAT format, void* pixeldata);
+}
+
+struct SMITHCALLS
+{
+	dExecuteCOG ExecuteCOG;
+	dIsInGame IsInGame;
+};
+
+
+// SYMBOLS U SHOULD EXPORT FOR A GREAT AND HAPPY PLUGIN
+
+// REQUIRED:
+// smith uses this to check if a DLL is a valid smith plugin. you must populate name
+// and u must return 1337 from this function in order for smith to proceed in recognizing your plugin.
+//
+// NOTE: smith MAY only TEMPORARILY load this plugin DLL in order to call SmithQueryPlugin.
+// if smith decides to actually use the plugin later on, it will then reload it a 2nd time
+// and call InitializePlugin to get the ball rolling.
+//int __cdecl SmithQueryPlugin(PLUGININFO& p)
+
+// at the point smith decides it will load your plugin during the session, it will load this plugin dll again (if not already loaded)
+// and call InitializePlugin, providing you with a pointer to the SMITHCALLS struct that contains all the available
+// smith engine calls that you will have access to throughout the remainder of the session.
+//int __cdecl InitializePlugin(SMITHCALLS* _smith)
+
+// once the session is over, smith will call ShutdownPlugin to allow you to gracefully tie up any loose ends.
+// generally, this will be followed by actually unloading this plugin DLL; however that's not gauranteed.
+// if smith supports a more dynamic plugin load/unload system then its possible sessions can be started/stopped
+// multiple times without the plugin being unloaded. so bear that in mind:  plz do not leak.
+// 
+// long story short;  kill any possible tasks you have that may either call smith API, or expect to be called by smith API.
+// forget the SMITHCALLS interface; its not gauranteed to be valid anymore.
+// the only functions smith will call after this point is either SmithQueryPlugin, or InitializePlugin again.
+//void __cdecl ShutdownPlugin()
+
+
+//-Called by the engine every cycle of the engine's main loop.
+//- AArcade uses this to perform its own main loop.
+//
+// dt = deltatime, intended for use in all simulation and engine tasks. may be conditioned eg. if game runs worse than 20fps then dt will be clamped to 1/20.  as well, if there is any kind of world slowmo, this dt will be augmented to include that
+// truedt = actual deltatime since last frame. please do not use this unless you really know why you need it over dt
+//void __cdecl OnMainLoop(double dt, double truedt)
+
+//-Called by the engine before it starts cleaning up any world objects from memory.
+//- AArcade uses this to tell internal mutli - thread systems that they should be suspended.
+//
+//NOTE:  Shutdown functions may be called multiple times, or called even if a corresponding Start was never called.
+//void __cdecl OnLevelShutdownPreObject()
+
+//-Called by the engine after all objects are removed from memory.
+//- AArcade uses this to perform general clean - up between sessions.
+//
+//NOTE:  Shutdown functions may be called multiple times, or called even if a corresponding Start was never called.
+//void __cdecl OnLevelShutdownPostObject()
+
+//-Called by the engine after the map is loaded but prior to any objects being created in it.
+//- AArcade uses this to perform general start - up logic for the session.
+//
+//NOTE: Smith promises to call this 1 time, and will not call again until the corresponding Shutdown functions have been called
+//void __cdecl OnLevelStartPreObject()
+
+//-Called by the engine after all world - objects are spawned into the world & the world simulation has begun.
+//- AArcade uses this to trigger its own start logic, which includes spawning many objects into the world.
+//
+////NOTE: Smith promises to call this 1 time, and will not call again until the corresponding Shutdown functions have been called
+//void __cdecl OnLevelStartPostObject()
+
+// called by smith whenever it is ready to execute a cog verb.  mods get first dibs.
+// returning 1 indicates to smith that the verb was handled and it will not be passed to any other
+// plugins or handled by smith itself.  returning 0 allows the verb to continue until its hopefully picked up by something else.
+// 
+// note: verb name in szName is always lowercase
+//int __cdecl OnExecuteCOGVerb(const char* szName, int nNumParams, const char** pszParams, char* szReturn)
